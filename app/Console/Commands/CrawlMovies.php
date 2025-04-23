107d70146eeaf69b74de42c36c95bcdab493a71b
Nguyen Trong Hieu <hieu745233@gmail.com>
2025-04-23 22:16:52 +0700

    feat: set total page

--- app/Console/Commands/CrawlMovies.php
+++ app/Console/Commands/CrawlMovies.php
@@ -45,7 +45,7 @@ class CrawlMovies extends Command
     public function handle()
     {
         $url = Config::get('crawler.movies_url');
-        $pages = 10;
+        $pages = 1214;
         $this->info('Starting movie data crawl...');
 
         if (CrawlerService::isBlockedByRobotsTxt($url)) {
