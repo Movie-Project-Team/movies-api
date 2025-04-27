6e5e3690fdbdfe95bc2c7efe40c1aeefc3833820
Nguyen Trong Hieu <hieu745233@gmail.com>
2025-04-27 09:48:37 +0700

    update code

--- routes/channels.php
+++ routes/channels.php
@@ -23,11 +23,7 @@ use Illuminate\Support\Facades\Cache;
 Broadcast::channel('room.{id}', function ($user, $id) {
     $profileId = Cache::get('verified_profile_for_user_' . $user->id);
     $profile = CommonService::getModel('Profile')->getDetail($user->id);
-
-    return [
-        'id' => 1,
-        'name' => "Nguye van A",
-    ];
+    return new ProfileResource($profile);
 });
 
 
